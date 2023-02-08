03f640b5b250b4fb60c6529f96569c249d89041b
Ruslan <rusishss@mail.ru>
2023-02-08 08:32:46 +0600

    second commit

---
+++ src/middlewares/rate-limiter.ts
@@ -0,0 +1,12 @@
+import expressRateLimit from 'express-rate-limit';
+
+export function createRateLimiter() {
+    return expressRateLimit({
+        windowMs: 10 * 1 * 1000, // 10 sec
+        max: 5, // limit each IP to 5 requests per windowMs
+        message: 'Too many requests, please try again later',
+        handler: (req, res) => {
+            res.status(429).send({ error: 'Too many requests, please try again later' });
+        }
+    });
+};
